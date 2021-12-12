3b82ec0a6b3168d139104e5f58ef5a5fd5c6792a
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2021-12-12 12:54:43 +0300

    scene edited

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -28,6 +36,8 @@ public class HelloApplication extends Application {
 
     Group group1 = new Group();
     Scene scene2 = new Scene(group1, 350, 500);
+
+    Alert a = new Alert(Alert.AlertType.NONE);
 
 
     private Pane createContent(){
@@ -129,8 +139,19 @@ public class HelloApplication extends Application {
                 return;
 
             if(hasBomb){ //game over
+                a.setAlertType(Alert.AlertType.CONFIRMATION);
+                a.setHeaderText("Game Over");
+                //a.setContentText("Game Over");
+                // show the dialog
+                a.show();
                 System.out.println("Game Over");
-                scene.setRoot(createContent());
+
+                isOpen = true;
+                text.setVisible(true);
+                text.setFill(Color.YELLOW);
+                border.setFill(null);
+
+                //scene.setRoot(createContent());
                 return;
             }
 
@@ -148,10 +169,122 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        scene = new Scene(createContent());
+        scene2 = new Scene(createContent());
 
-        stage.setScene(scene);
-        stage.show();
+        Stage stage2 = new Stage();
+
+         stage2.setScene(scene2);
+
+
+
+
+        /////
+
+
+
+            Pane pane = new Pane();
+			/*String x="Welcome to Escape from the Bomb";
+			Label []enter =new Label[31];
+
+			for(int i=0;i<31;i++) {
+				enter[i].setText(String.valueOf(x.charAt(i)));
+				pane.add(e, i, i, i, i);
+			}
+			*/
+            Label label = new Label();
+            label.setText("Welcome to Escape from the Bomb");
+            label.setFont(Font.font("Times New Roman", FontPosture.REGULAR, 20));
+            label.setTextFill(Color.WHITE);
+            Label label2 = new Label();
+            label2.setFont(Font.font("Times New Roman", FontPosture.ITALIC, 10));
+            label2.setText("(Please select a game board)");
+            label2.setTextFill(Color.WHITE);
+
+
+            //pane.getChildren().add(label);
+            //label.setAlignment(Pos.TOP_CENTER);
+
+
+            //enter.setPrefSize(20, 20);
+            Button ten = new Button("10x10");
+            Button fifteen = new Button("15x15");
+            Button twenty = new Button("20x20");
+
+
+            //enter.setPadding(new Insets(10,10,10,10));
+
+            //pane.addRow(5, enter);
+            //pane.getChildren().add(enter);
+
+
+            ten.setFont(Font.font("Helvetica  ", FontWeight.BOLD, 15));
+            fifteen.setFont(Font.font("Helvetica  ", FontWeight.BOLD, 15));
+            twenty.setFont(Font.font("Helvetica", FontWeight.BOLD, 15));
+
+			/*ten.setStyle("-fx-background-color: lightgreen");
+			fifteen.setStyle("-fx-background-color: yellow");
+			twenty.setStyle("-fx-background-color: red");*/
+
+
+			/*ten.setStyle("-fx-border-color: black");
+			fifteen.setStyle("-fx-border-color: black");
+			twenty.setStyle("-fx-border-color: black");
+			*/
+            pane.getChildren().addAll(label, label2, ten, fifteen, twenty);
+			/*pane.setPadding(new Insets(30, 30, 30, 30));
+			pane.setVgap(60);
+			pane.setHgap(81); //yanlamasına aralık
+			*/
+
+
+            label.relocate(75, 25);
+            label2.relocate(160, 150);
+            ten.relocate(35, 175);
+            fifteen.relocate(190, 175);
+            twenty.relocate(345, 175);
+
+			/*pane.setTop(label);
+			pane.setCenter(ten);
+			pane.setCenter(fifteen);
+			pane.setCenter(twenty);
+
+			ten.setAlignment(Pos.CENTER_LEFT);
+			fifteen.setAlignment(Pos.CENTER);
+			twenty.setAlignment(Pos.CENTER_RIGHT);
+			*/
+
+
+			/*pane.setVgap(10);
+			pane.setHgap(10);
+			//pane.add(enter, 3, 1);
+			pane.add(ten, 5, 20);
+			pane.add(fifteen, 10, 20);
+			pane.add(twenty,15,20);*/
+            Image image = new Image("https://mpng.subpng.com/20180306/ljw/kisspng-bomb-weapon-cartoon-bomb-vector-hand-painted-5a9e7906dc2fd7.4838742115203351109019.jpg");
+
+            pane.setStyle("-fx-background-image :url(https://thumbs.dreamstime.com/b/minefield-mines-explosives-bombs-background-illustration-full-fuse-lit-each-bomb-going-to-go-boom-87753649.jpg)");
+
+
+            //pane.getChildren().addAll(ten,fifteen,twenty);
+
+            //Image image=new Image("https://mpng.subpng.com/20180306/ljw/kisspng-bomb-weapon-cartoon-bomb-vector-hand-painted-5a9e7906dc2fd7.4838742115203351109019.jpg");
+            ImageView imageview = new ImageView(image);
+
+
+
+            ten.addEventHandler(ActionEvent.ACTION, e -> {
+                stage2.show();
+            });
+
+            Scene scene1 = new Scene(pane, 430, 400);
+
+            stage.setResizable(false);
+            stage.getIcons().add(image);
+            stage.setTitle("Escape From the Bomb");
+            stage.setScene(scene1);
+            stage.show();
+
+
 
     }
 
